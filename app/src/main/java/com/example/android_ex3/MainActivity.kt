5ea1d73ef45a63004d05af5d076cfcb74f11c0f2
amir feiz <amir.feiz.1381@gmail.com>
2024-05-24 18:47:17 +0330

    project created

---
+++ app/src/main/java/com/example/android_ex3/MainActivity.kt
@@ -0,0 +1,15 @@
+// MainActivity.kt
+package com.example.networkmonitor
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+
+}
+
